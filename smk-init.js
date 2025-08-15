d37d723cc5632dcf2f245f9469065e0da3e96b39
Graham MacGregor <graham.macgregor@gov.bc.ca>
2025-08-15 14:26:07 -0700

    test55

--- smk-init.js
+++ smk-init.js
@@ -1,6 +1,8 @@
 SMK.INIT( {
     containerSel: '#smk-map-frame',
-    config: [ './smk-config.json', '?' ]
+    config: [ './smk-config.json', '?' ],
+    baseUrl:        'smk/dist',
+    id:             'my-map'
 } )
 .then( function ( smk ) {
     // SMK initialized
